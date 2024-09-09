d68315bcbe8a67e832d1061939bcdf81c8be6bfb
Walid <shokriw@gmail.com>
2024-09-09 15:08:41 +0300

    Create Header.js

---
+++ Header.js
@@ -0,0 +1,13 @@
+// src/components/Header.js
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+  return (
+    <header>
+      <h1><Link to="/">Riyadh Jobs</Link></h1>
+    </header>
+  );
+};
+
+export default Header;
